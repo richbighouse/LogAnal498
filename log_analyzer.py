1470dde28063dca75d30fdfe618b3c89a5198b55
richbighouse <rich.grandmaison@gmail.com>
2017-04-15 18:13:45 -0400

    question1

--- log_analyzer.py
+++ log_analyzer.py
@@ -9,16 +9,38 @@ from os.path import isfile, join
 from pyspark import SparkContext, SparkConf
 
 logdir = os.getcwd() + "/logs/"
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext(conf = conf)
 
 def main(argv):
 	checkvalid(sys.argv)
-	print(logdir)
+	if(int(sys.argv[2]) == 1):
+		question1(sys.argv)
+
 
 def checkvalid(argv):
 	r = range(1,10)
 	if(not(argv[1] == "-q") or not(int(argv[2]) in r)):
 		print "command malformed! expecting -q [1-9]"
-		sys.exit()	
+		sys.exit()
+	for x in xrange (3, len(argv)):
+		if(not(os.path.isdir(logdir + argv[x]))):
+			print "argument not a dir!"	
+			sys.exit()
+def question1(argv):
+	print("* Q1: line counts")
+	count = 0
+	for x in xrange (3, len(argv)):
+		count = 0
+		dirname = argv[x]
+		for file in os.listdir(logdir + dirname):
+			text_file = sc.textFile(logdir + dirname + "/" + file)
+			count += text_file.count()
+		print("+ " + dirname + ": " + str(count))
+
 
 if __name__ == "__main__":
 	main(sys.argv)

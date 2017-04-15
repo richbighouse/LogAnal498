b2473bf153a636268c247d5b197938dfd9aee85b
richbighouse <rich.grandmaison@gmail.com>
2017-04-15 16:18:04 -0400

    file checking

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,11 +3,22 @@
 """
 
 import sys
-from pyspark import SparkContext
+import os
+from os import walk
+from os.path import isfile, join
+from pyspark import SparkContext, SparkConf
+
+logdir = os.getcwd() + "/logs/"
 
 def main(argv):
+	checkvalid(sys.argv)
+	print(logdir)
 
-	
+def checkvalid(argv):
+	r = range(1,10)
+	if(not(argv[1] == "-q") or not(int(argv[2]) in r)):
+		print "command malformed! expecting -q [1-9]"
+		sys.exit()	
 
 if __name__ == "__main__":
 	main(sys.argv)

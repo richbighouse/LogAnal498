de0014fa74253f709ca41cd58c4f67d1faa7adf0
rich <rich.grandmaison@gmail.com>
2017-04-15 14:03:42 -0400

    first commit

---
+++ log_analyzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""log_analyzer.py, by Simon and Richard
+"""
+
+import sys
+from pyspark import SparkContext
+
+def main(argv):
+
+	
+
+if __name__ == "__main__":
+	main(sys.argv)
+
+
